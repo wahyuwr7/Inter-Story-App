e77dd544412b773f81607be119e9780f39ebf30f
wahyuwr7 <wahyu.wrseven@gmail.com>
2022-08-24 14:34:30 +0800

    First Init

---
+++ app/src/main/java/com/yura/interstoryapp/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.yura.interstoryapp.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yura.interstoryapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
